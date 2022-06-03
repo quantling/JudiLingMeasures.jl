cef3a08da6529519ee40410935e69cb7d1d0caef
MariaHei <mabuwu83@gmail.com>
2022-06-03 15:24:49 +0200

    Added installation of pyldl and pandas

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,19 @@ using Statistics
 using Test
 using Distances
 using PyCall
+import Conda
 
 # Test scripts
 include("test_helpers.jl")
 include("test_measures.jl")
+
+import Conda
+
+function __init__()
+    if !haskey(Conda._installed_packages_dict(),"pandas")
+        Conda.add("pandas")
+    end
+    if !haskey(Conda._installed_packages_dict(),"numpy")
+        Conda.add("numpy")
+    end
+end
