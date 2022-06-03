45e4b07b4c61d19207207cafaccb934edc3e7256
MariaHei <mabuwu83@gmail.com>
2022-06-03 15:38:46 +0200

    Another possibility of adding pyldl?

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,20 +10,16 @@ using Distances
 using PyCall
 import Conda
 
+if !haskey(Conda._installed_packages_dict(),"pandas")
+    Conda.add("pandas")
+end
+if !haskey(Conda._installed_packages_dict(),"numpy")
+    Conda.add("numpy")
+end
+if !haskey(Conda._installed_packages_dict(),"pyldl")
+    Conda.add("pyldl", channel="https://github.com/msaito8623/pyldl")
+end
+
 # Test scripts
 include("test_helpers.jl")
 include("test_measures.jl")
-
-import Conda
-
-function __init__()
-    if !haskey(Conda._installed_packages_dict(),"pandas")
-        Conda.add("pandas")
-    end
-    if !haskey(Conda._installed_packages_dict(),"numpy")
-        Conda.add("numpy")
-    end
-    if !haskey(Conda._installed_packages_dict(),"pyldl")
-        Conda.add("pyldl", channel="https://github.com/msaito8623/pyldl")
-    end
-end
