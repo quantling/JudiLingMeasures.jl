026c07f08d8b792d143c4c8f15e74e37e2b66535
Maria Heitmeier <mabuwu83@gmail.com>
2026-01-18 10:17:43 +0000

    Make package compatible with Julia 1.12 and use CondaPkg and PythonCall instead of Conda and PyCall

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,18 +7,7 @@ using LinearAlgebra
 using Statistics
 using Test
 using Distances
-using PyCall
-import Conda
-
-# if !haskey(Conda._installed_packages_dict(),"pandas")
-#     Conda.add("pandas")
-# end
-# if !haskey(Conda._installed_packages_dict(),"numpy")
-#     Conda.add("numpy")
-# end
-# if !haskey(Conda._installed_packages_dict(),"pyldl")
-#     Conda.add("pyldl", channel="https://github.com/msaito8623/pyldl")
-# end
+using PythonCall
 
 # Test scripts
 include("test_helpers.jl")
